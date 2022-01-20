23b52435bdfe7ac0bd051416ddff8e03c078d718
Elena Vorobjeva <vorobjeva556@yandex.ru>
2022-01-20 20:28:45 +0300

    v1

--- handler.js
+++ handler.js
@@ -1,43 +1,55 @@
-let playButton    = document.querySelector(".play");
-let pauseButton   = document.querySelector(".pause");
-let clearButton   = document.querySelector(".clear");
+let playButton = document.querySelector(".play");
+let pauseButton = document.querySelector(".pause");
+let clearButton = document.querySelector(".clear");
 let settingButton = document.querySelector(".setting-btn");
-let mainBlock     = document.querySelector("main");
-let settingBlock  = document.querySelector(".setting");
-let minutesBlock  = document.querySelector(".minutes");
-let secondsBlock  = document.querySelector(".seconds");
+let mainBlock = document.querySelector("main");
+let settingBlock = document.querySelector(".setting");
+let minutesBlock = document.querySelector(".minutes");
+let secondsBlock = document.querySelector(".seconds");
 let timeBlock = document.querySelector(".time-block");
 let currentIntervalBlock = document.querySelector(".current-interval");
 let allIntervalsBlock = document.querySelector(".all-intervals");
+let currentRoundIntervalBlock = document.querySelector(".current-round-interval");
+let allRoundIntervalsBlock = document.querySelector(".all-round-intervals");
 let taskBlock = document.querySelector(".task");
-let closeButton   = document.querySelector(".close");
+let closeButton = document.querySelector(".close");
 
-settingButton.onclick = function() {
-    settingBlock.style.display='block';
-    mainBlock.style.display='none';
+settingButton.onclick = function () {
+    settingBlock.style.display = 'block';
+    mainBlock.style.display = 'none';
 }
 
-closeButton.onclick = function() {
-    mainBlock.style.display='flex';
-    settingBlock.style.display='none';
+closeButton.onclick = function () {
+    mainBlock.style.display = 'flex';
+    settingBlock.style.display = 'none';
+}
+
+function setInitialValue() {
+    document.querySelector("#work-time").value = 25;
+    document.querySelector("#short-break-time").value = 5;
+    document.querySelector("#long-break-time").value = 30;
+    document.querySelector("#work-in-round").value = 4;
+    document.querySelector("#work-at-the-day").value = 12;
 }
 
 function initialState() {
+    setInitialValue();
     minutesBlock.innerHTML = leadZero(document.querySelector("#work-time").value);
     secondsBlock.innerHTML = "00";
+    currentRoundIntervalBlock.innerHTML = "1";
+    allRoundIntervalsBlock.innerHTML = document.querySelector("#work-in-round").value;
     currentIntervalBlock.innerHTML = "1";
     allIntervalsBlock.innerHTML = document.querySelector("#work-at-the-day").value;
     taskBlock.innerHTML = "Время поработать!";
-
 }
 
 initialState();
 
 let now_seconds = 0; // сколько секунд прошло
+let now_round_times = 0;
 let now_times = 0;
 let interval_type = 'work'; // режим работы таймера
 let intervalVariable; // id таймера
-let seconds_1 = 0; // количество секунд в таймере
 let timer_minutes = 0; // начальные значения счетчиков минут
 let timer_seconds = 0; // --//-- секунд
 
@@ -46,7 +58,7 @@ function secondsToTime(seconds) {
     let s = seconds % 60;
 
     return {
-        'minutes': leadZero(m), 
+        'minutes': leadZero(m),
         'seconds': leadZero(s)
     };
 }
@@ -63,42 +75,34 @@ function renderTimerNums(seconds) {
 }
 
 function timerTick(timer_params) {
-    console.log("now_seconds = " + now_seconds);
-    if(interval_type == 'work') {
-        console.log("if interval type is work");
-        if(timer_params.time_work - now_seconds > 0) {
-            console.log('time to work is not end yet');
-            console.log("timer_params.time_work - now_seconds = " + (timer_params.time_work - now_seconds));
+    if (interval_type == 'work') {
+        if (timer_params.time_work - now_seconds > 0) {
             timeBlock.style.color = '#F6FB00'; //yellow
             taskBlock.innerHTML = "Время поработать!";
             renderTimerNums(timer_params.time_work - now_seconds);
             now_seconds++;
         }
         else {
-            console.log('time to work is end');
             renderTimerNums(0);
             now_seconds = 0;
-            interval_type = 'short_rest';
+            if (now_times % timer_params.round_interval_count == 0) interval_type = 'long_rest'
+            else interval_type = 'short_rest';
         }
     }
-    else if(interval_type == 'short_rest') {
-        console.log("if interval type is short rest");
-        if(timer_params.time_short_rest - now_seconds > 0) {
-            console.log('time to rest is not end yet');
-            console.log("timer_params.time_short_rest - now_seconds = " + (timer_params.time_short_rest - now_seconds));
+    else if (interval_type == 'short_rest') {
+        if (timer_params.time_short_rest - now_seconds > 0) {
             timeBlock.style.color = '#00FB19'; //green
             taskBlock.innerHTML = "Время отходнуть!";
             renderTimerNums(timer_params.time_short_rest - now_seconds);
             now_seconds++;
         }
         else {
-            console.log('time to rest is end');
             renderTimerNums(0);
             now_seconds = 0;
+            now_round_times++;
             now_times++;
-            console.log("now_times = " + now_times);
-            if(now_times > timer_params.interval_count) {
-                console.log("all round is end");
+            if (now_times > timer_params.interval_count) {
+                currentRoundIntervalBlock.innerHTML = timer_params.round_interval_count;
                 currentIntervalBlock.innerHTML = timer_params.interval_count;
                 let event = new Event("click");
                 pauseButton.dispatchEvent(event);
@@ -106,7 +110,34 @@ function timerTick(timer_params) {
                 timeBlock.style.color = '#FFFFFF';
             }
             else {
-                console.log("all round is not end");
+                currentRoundIntervalBlock.innerHTML = now_round_times;
+                currentIntervalBlock.innerHTML = now_times;
+            }
+            interval_type = 'work';
+        }
+    }
+    else if (interval_type == 'long_rest') {
+        if (timer_params.time_long_rest - now_seconds > 0) {
+            timeBlock.style.color = '#00FB19'; //green
+            taskBlock.innerHTML = "Время длинного перерыва!";
+            renderTimerNums(timer_params.time_long_rest - now_seconds);
+            now_seconds++;
+        }
+        else {
+            renderTimerNums(0);
+            now_seconds = 0;
+            now_round_times = 1;
+            now_times++;
+            if (now_times > timer_params.interval_count) {
+                currentRoundIntervalBlock.innerHTML = timer_params.round_interval_count;
+                currentIntervalBlock.innerHTML = timer_params.interval_count;
+                let event = new Event("click");
+                pauseButton.dispatchEvent(event);
+                now_seconds = 0;
+                timeBlock.style.color = '#FFFFFF';
+            }
+            else {
+                currentRoundIntervalBlock.innerHTML = now_round_times;
                 currentIntervalBlock.innerHTML = now_times;
             }
             interval_type = 'work';
@@ -114,45 +145,32 @@ function timerTick(timer_params) {
     }
 }
 
-playButton.onclick = function() {
-    playButton.style.display='none';
-    pauseButton.style.display='block';
+playButton.onclick = function () {
+    playButton.style.display = 'none';
+    pauseButton.style.display = 'block';
 
     timer_minutes = minutesBlock.innerHTML;
     timer_seconds = secondsBlock.innerHTML;
+    now_round_times = +currentRoundIntervalBlock.innerHTML;
     now_times = +currentIntervalBlock.innerHTML;
-    console.log("timer_minutes = " + timer_minutes);
-    console.log("timer_seconds = " + timer_seconds);
-    console.log("now_times = " + now_times);
 
     let timer_params = {};
     timer_params.time_work = document.querySelector("#work-time").value * 60;
     timer_params.time_short_rest = document.querySelector("#short-break-time").value * 60;
+    timer_params.time_long_rest = document.querySelector("#long-break-time").value * 60;
+    timer_params.round_interval_count = +document.querySelector("#work-in-round").value;
     timer_params.interval_count = +document.querySelector("#work-at-the-day").value;
 
-    console.log("timer_params.time_work = " + timer_params.time_work);
-    console.log("timer_params.time_short_rest = " + timer_params.time_short_rest);
-    console.log("timer_params.interval_count = " + timer_params.interval_count);
+    if (now_round_times >= timer_params.round_interval_count) {
+        now_round_times = 1;
+        currentRoundIntervalBlock.innerHTML = now_round_times;
+    }
 
-    
-
-    if(now_times >= timer_params.interval_count) {
-        console.log("if all of rounds are end");
+    if (now_times >= timer_params.interval_count) {
+        now_round_times = 1;
         now_times = 1;
+        currentRoundIntervalBlock.innerHTML = now_round_times;
         currentIntervalBlock.innerHTML = now_times;
-    }
-
-    if(interval_type == 'work') {
-        console.log("if interval type is work");
-        timeBlock.style.color = '#F6FB00'; //yellow
-        seconds_1 = timer_params.time_work;
-        console.log("seconds_1 = " + seconds_1);
-    }
-    else if(interval_type == 'short_rest') {
-        console.log("if interval type is short rest");
-        timeBlock.style.color = '#00FB19'; //green
-        seconds_1 = timer_params.time_short_rest;
-        console.log("seconds_1 = " + seconds_1);
     }
 
     intervalVariable = setInterval(timerTick, 1000, timer_params);
@@ -160,16 +178,16 @@ playButton.onclick = function() {
     return false;
 }
 
-pauseButton.onclick = function(event, params) {
-    pauseButton.style.display='none';
-    playButton.style.display='block';
+pauseButton.onclick = function () {
+    pauseButton.style.display = 'none';
+    playButton.style.display = 'block';
 
     clearInterval(intervalVariable);
 
     return false;
 }
 
-clearButton.onclick = function() {
+clearButton.onclick = function () {
     let event = new Event("click");
     pauseButton.dispatchEvent(event);
 
@@ -182,3 +200,20 @@ clearButton.onclick = function() {
 
     return false;
 }
+
+
+document.querySelectorAll(".minus").forEach(function (minusBlock) {
+    minusBlock.addEventListener("click", function () {
+        let input = minusBlock.parentNode.querySelector("input");
+        if (input.value > 1) --input.value;
+    });
+});
+
+document.querySelectorAll(".plus").forEach(function (plusBlock) {
+    plusBlock.addEventListener("click", function () {
+        let input = plusBlock.parentNode.querySelector("input");
+        if (input.value < 60) ++input.value;
+    });
+});
+
+document.querySelector(".clear-setting").addEventListener("click", setInitialValue);
